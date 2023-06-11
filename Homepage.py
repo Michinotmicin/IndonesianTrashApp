62661a5610c5d41bf8deb29e9ee59c0c8bc2a07e
Michelle Tsjin <92587333+Michinotmicin@users.noreply.github.com>
2023-06-12 05:12:38 +0800

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -192,7 +192,7 @@ if selected == "Dashboards":
         st.write("")
 
     with col5:
-        st.markdown("###### South Korea Recycling System")
+        st.markdown("##### South Korea Recycling System")
         st.markdown("South Korean government is implementing **Extended Procedure Responsibility (EPR)** \
                     which is about strengthening the producer's responsibility from the production stage up to collection \
                     and recycling. What type of packaging or trash the EPR system is applied? The EPR system is applied to \
